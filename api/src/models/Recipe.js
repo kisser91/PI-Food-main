03ff99544007186547feeb4b856063dcfe1f1fef
NicolasMarianoMorel <nicolasmorel77@gmail.com>
2021-11-08 19:05:22 -0300

    Database ready

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+      get() {
+        return `L${this.getDataValue('id')}`
+      },
+      set(value){
+        this.setDataValue('id', value.slice(1) * 1)
+      }
+    },
     name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
-  });
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    points: {
+      type: DataTypes.STRING,
+    },
+    healthScore: {
+      type: DataTypes.STRING
+    },
+    steps: {
+      type: DataTypes.TEXT
+    },
+  }, {timestamps: false} );
 };
